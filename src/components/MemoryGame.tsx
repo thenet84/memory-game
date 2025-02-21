0208aa51a5f444fa278d3b480764a0c6870037f2
thenet84 <the.net.wo@gmail.com>
2025-02-21 14:05:09 +0100

    feat: handle match

--- src/components/MemoryGame.tsx
+++ src/components/MemoryGame.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './MemoryGame.css'
+import { shuffle } from '../utils/shuffle';
 
 type MemoryGameProps = {
   images: string[];
@@ -13,7 +14,7 @@ type Card = {
 
 function MemoryGame({ images }: MemoryGameProps) {
   const [cards, setCards] = useState<Card[]>(() => {
-    const gameImages = [...images, ...images];
+    const gameImages = shuffle<string>([...images, ...images]);
     return gameImages.map((image, index) => ({
       id: index,
       imageUrl: image,
@@ -22,17 +23,52 @@ function MemoryGame({ images }: MemoryGameProps) {
     }))
   })
 
+
+  function handleFlippedCards(firstCard: Card, secondCard: Card) {
+    if (firstCard.imageUrl == secondCard.imageUrl) {
+      setCards((previousCards) => {
+        const newCards = [...previousCards];
+        newCards[firstCard.id].isMatched = true;
+        newCards[secondCard.id].isMatched = true;
+        return newCards;
+      });
+    }
+    else {
+      setTimeout(() => {
+        setCards((previousCards) => {
+          const newCards = [...previousCards];
+          newCards[firstCard.id].isFlipped = false;
+          newCards[secondCard.id].isFlipped = false;
+          return newCards;
+        })
+      }, 800)
+    }
+  }
+
   function handleCardClick(card: Card) {
-    if (card.isFlipped || card.isMatched) {
+    if (card.isMatched) {
+      return;
+    }
+    if (card.isFlipped) {
+      setCards((previousCards) => {
+        const newCards = [...previousCards];
+        newCards[card.id].isFlipped = false;
+        return newCards;
+      });
       return;
     }
     setCards((previousCards) => {
       const newCards = [...previousCards];
       newCards[card.id].isFlipped = true;
+
+      const flippedCards = newCards.filter(({ isFlipped, isMatched }) => isFlipped && !isMatched);
+
+      if (flippedCards.length == 2) {
+        handleFlippedCards(flippedCards[0], flippedCards[1])
+      }
       return newCards;
     })
   }
-  
 
   return (
     <div className="memory-game">
